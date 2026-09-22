c85d6a489978ff05125b1cbe5de9d06556382b70
ykpyl-glitch <measanehill@gmail.com>
2026-09-22 17:54:59 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,3 @@
+const CACHE="notes-v1";
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json","./apple-touch-icon.png"]))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
